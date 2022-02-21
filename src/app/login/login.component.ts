4443f3af4e29faf58240bdcf5633bd99293aaae9
fourneau <68759271+fourneau@users.noreply.github.com>
2022-02-21 15:07:20 +0100

    Formulaires validator loginok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
   faUser = faUser;
   faKey = faKey;
   loginForm!: FormGroup;
+  submitted!: boolean;
   
   constructor(private fb: FormBuilder) {}
 
@@ -20,10 +21,28 @@ export class LoginComponent implements OnInit {
     this.loginForm = this.fb.group({
       username: ['',Validators.required],
       password: ['',[Validators.required,Validators.minLength(6)]],
+    },
+    {
+      validator: MustMatch('password','password')
     });
   }
   login() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+     return alert('errors!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
+      
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
     console.log('Données du formulaire', this.loginForm.value)
-  }
+}
+
+onReset() {
+    this.submitted = false;
+    this.loginForm.reset();
+}
 
 }
